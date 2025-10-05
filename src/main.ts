ead3aeb9db541ddf3be5520c8437ee5e8395778b
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-10-05 11:56:08 +0200

    register syncfusion

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
+import { registerLicense } from '@syncfusion/ej2-base';
+import { environment } from './environments/environment';
+
+// Registering Syncfusion license key
+registerLicense(environment.SYNCFUSION_LICENSE);
 
 registerLocaleData(fr.default);
 
